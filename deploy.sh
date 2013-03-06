2dd9de7367958de3eacac90721f6b9c8aeedee6a
dennywu0315 <dennywu0315@gmail.com>
2013-03-06 16:35:00 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd /root/dev/myblog
+
+pelican -s pelicanconf.py content -o myblog 
+
+cd myblog
+
+git add .
+git commit -m 'new Article posted by script'
+git push
+
---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'DennyW'
+SITENAME = u'MarukoHome'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'en'
+
+THEME = 'bootstrap2'
+FEED_DOMAIN = 'http://www.marukohome.com'
+
+# Blogroll
+LINKS =  (('Zhihu', 'http://www.zhihu.com/'),
+          ('Fenng', 'http://dbanotes.net/'),
+          ('JobsDigg.com', 'https://jobsdigg.com/'),
+          ('Hacker News', 'http://news.ycombinator.com/'),
+          ('Startup News', 'http://news.dbanotes.net/news'),
+          ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),)
+
+# Social widget
+SOCIAL = (('Weibo', 'http://weibo.com/u/1652634905'),
+          ('GitHub', 'https://github.com/dennywu0315'),)
+
+DEFAULT_PAGINATION = 10
+
